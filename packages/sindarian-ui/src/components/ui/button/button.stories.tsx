56da02340b46d0aac62645171bac9f620af0cdf2
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2025-08-29 16:59:33 -0300

    feat: Adjusted button component

--- packages/sindarian-ui/src/components/ui/button/button.stories.tsx
+++ packages/sindarian-ui/src/components/ui/button/button.stories.tsx
@@ -9,41 +9,42 @@ const meta: Meta<ButtonProps> = {
     children: {
       type: 'string',
       description: "The button's content"
-    },
-    disabled: {
-      type: 'boolean',
-      description: 'If the button is disabled'
-    },
-    className: {
-      type: 'string',
-      description: "The button's class"
     }
   }
 }
 
 export default meta
 
-export const Primary: StoryObj<ButtonProps> = {
+export const Component: StoryObj<ButtonProps> = {
   args: {
     children: 'Button'
   }
 }
 
-export const Disabled: StoryObj<ButtonProps> = {
+export const Primary: StoryObj<ButtonProps> = {
   args: {
-    children: 'Button',
-    disabled: true
-  }
+    children: 'Button'
+  },
+  render: (args) => (
+    <div className="flex gap-2">
+      <Button {...args} /> <Button {...args} disabled />
+    </div>
+  )
 }
 
-export const Secundary: StoryObj<ButtonProps> = {
+export const Secondary: StoryObj<ButtonProps> = {
   args: {
     children: 'Button',
     variant: 'secondary'
-  }
+  },
+  render: (args) => (
+    <div className="flex gap-2">
+      <Button {...args} /> <Button {...args} disabled />
+    </div>
+  )
 }
 
-export const SecundaryDisabled: StoryObj<ButtonProps> = {
+export const SecondaryDisabled: StoryObj<ButtonProps> = {
   args: {
     children: 'Button',
     variant: 'secondary',
@@ -51,10 +52,33 @@ export const SecundaryDisabled: StoryObj<ButtonProps> = {
   }
 }
 
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Button',
+    variant: 'tertiary'
+  },
+  render(args) {
+    return (
+      <div className="flex gap-2">
+        <Button {...args} />
+        <Button {...args} disabled />
+      </div>
+    )
+  }
+}
+
 export const Outline: StoryObj<ButtonProps> = {
   args: {
     children: 'Button',
     variant: 'outline'
+  },
+  render(args) {
+    return (
+      <div className="flex gap-2">
+        <Button {...args} />
+        <Button {...args} disabled />
+      </div>
+    )
   }
 }
 
@@ -63,7 +87,14 @@ export const FullWidth: StoryObj<ButtonProps> = {
     fullWidth: true,
     children: 'Button'
   },
-  render: (args) => <Button {...args} />
+  render: (args) => (
+    <div className="flex flex-col gap-2">
+      <Button {...args} />
+      <Button variant="secondary" {...args} />
+      <Button variant="tertiary" {...args} />
+      <Button variant="outline" {...args} />
+    </div>
+  )
 }
 
 export const WithIcon: StoryObj<ButtonProps> = {
@@ -71,15 +102,110 @@ export const WithIcon: StoryObj<ButtonProps> = {
     children: 'Button'
   },
   render: (args) => (
-    <div className="flex flex-col gap-4">
-      <div className="flex flex-row gap-4">
-        <Button icon={<Users />} {...args} />
-        <Button icon={<Users />} {...args} />
-      </div>
-      <div className="flex flex-row gap-4">
+    <div className="grid grid-cols-4 gap-4">
+      <div className="flex flex-col gap-4">
+        <div>
+          <Button icon={<Users />} {...args} />
+        </div>
+        <div>
+          <Button icon={<Users />} iconPlacement="end" {...args} />
+        </div>
         <Button fullWidth icon={<Users />} {...args} />
         <Button fullWidth iconPlacement="far-end" icon={<Users />} {...args} />
       </div>
+      <div className="flex flex-col gap-4">
+        <div>
+          <Button variant="secondary" icon={<Users />} {...args} />
+        </div>
+        <div>
+          <Button
+            variant="secondary"
+            iconPlacement="end"
+            icon={<Users />}
+            {...args}
+          />
+        </div>
+        <Button variant="secondary" fullWidth icon={<Users />} {...args} />
+        <Button
+          variant="secondary"
+          fullWidth
+          iconPlacement="far-end"
+          icon={<Users />}
+          {...args}
+        />
+      </div>
+      <div className="flex flex-col gap-4">
+        <div>
+          <Button variant="tertiary" icon={<Users />} {...args} />
+        </div>
+        <div>
+          <Button
+            variant="tertiary"
+            iconPlacement="end"
+            icon={<Users />}
+            {...args}
+          />
+        </div>
+        <Button variant="tertiary" fullWidth icon={<Users />} {...args} />
+        <Button
+          variant="tertiary"
+          fullWidth
+          iconPlacement="far-end"
+          icon={<Users />}
+          {...args}
+        />
+      </div>
+      <div className="flex flex-col gap-4">
+        <div>
+          <Button variant="outline" icon={<Users />} {...args} />
+        </div>
+        <div>
+          <Button
+            variant="outline"
+            iconPlacement="end"
+            icon={<Users />}
+            {...args}
+          />
+        </div>
+        <Button variant="outline" fullWidth icon={<Users />} {...args} />
+        <Button
+          variant="outline"
+          fullWidth
+          iconPlacement="far-end"
+          icon={<Users />}
+          {...args}
+        />
+      </div>
+    </div>
+  )
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Button',
+    size: 'small'
+  },
+  render: (args) => (
+    <div className="flex gap-2">
+      <Button {...args} />
+      <Button variant="secondary" {...args} />
+      <Button variant="tertiary" {...args} />
+      <Button variant="outline" {...args} />
+    </div>
+  )
+}
+
+export const ReadOnly: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Button',
+    readOnly: true
+  },
+  render: (args) => (
+    <div className="flex gap-2">
+      <Button {...args} />
+      <Button variant="secondary" {...args} />
+      <Button variant="tertiary" {...args} />
+      <Button variant="outline" {...args} />
     </div>
   )
 }
