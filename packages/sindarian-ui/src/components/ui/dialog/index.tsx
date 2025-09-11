61af0b3b0e538c7ef19fbae4d3388ab167bc6d3e
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2025-09-11 12:30:54 -0300

    feat: More dialog adjusts

--- packages/sindarian-ui/src/components/ui/dialog/index.tsx
+++ packages/sindarian-ui/src/components/ui/dialog/index.tsx
@@ -37,10 +37,7 @@ function DialogOverlay({
   return (
     <DialogPrimitive.Overlay
       data-slot="dialog-overlay"
-      className={cn(
-        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/50',
-        className
-      )}
+      className={cn('dialog-overlay', className)}
       {...props}
     />
   )
@@ -60,7 +57,7 @@ function DialogContent({
       <DialogPrimitive.Content
         data-slot="dialog-content"
         className={cn(
-          'bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-8 border p-6 shadow-lg duration-200 sm:max-w-[425px] sm:rounded-lg',
+          'dialog-content',
           !showCloseButton && '*:data-radix-dialog-close:hidden',
           className
         )}
@@ -69,7 +66,7 @@ function DialogContent({
         {children}
         <DialogPrimitive.Close
           data-slot="dialog-close"
-          className="data-[state=open]:bg-accent absolute top-4 right-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 disabled:pointer-events-none data-[state=open]:text-gray-400"
+          className="dialog-close"
         >
           <X className="size-4" />
           <span className="sr-only">Close</span>
@@ -83,10 +80,7 @@ function DialogHeader({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       data-slot="dialog-header"
-      className={cn(
-        'flex flex-col gap-4 space-y-1.5 text-center sm:text-left',
-        className
-      )}
+      className={cn('dialog-header', className)}
       {...props}
     />
   )
@@ -96,10 +90,7 @@ function DialogFooter({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       data-slot="dialog-footer"
-      className={cn(
-        'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
-        className
-      )}
+      className={cn('dialog-footer', className)}
       {...props}
     />
   )
@@ -112,10 +103,7 @@ function DialogTitle({
   return (
     <DialogPrimitive.Title
       data-slot="dialog-title"
-      className={cn(
-        'text-lg leading-none font-bold tracking-tight text-zinc-600',
-        className
-      )}
+      className={cn('dialog-title', className)}
       {...props}
     />
   )
@@ -128,7 +116,7 @@ function DialogDescription({
   return (
     <DialogPrimitive.Description
       data-slot="dialog-description"
-      className={cn('text-shadcn-400 text-sm font-medium', className)}
+      className={cn('dialog-description', className)}
       {...props}
     />
   )
